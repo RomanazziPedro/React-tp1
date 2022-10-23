148711513def5f5aa8c0b4ee60951ddf1ba9bd28
RomanazziPedro <romanazzipedro@gmail.com>
2022-10-22 23:49:04 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Routes from "./pages/routes";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<CssBaseline>
+<BrowserRouter>
+<Routes />
+</BrowserRouter>
+</CssBaseline>
+);
 }
-
 export default App;
